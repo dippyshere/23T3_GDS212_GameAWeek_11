59332d211ae7fdb3d96b281b07239cb486188371
Alex Hanson <43489279+dippyshere@users.noreply.github.com>
2023-12-15 00:11:59 +1100

    implement switching camera between areas/rails

--- Assets/Scripts/CameraTrackController.cs
+++ Assets/Scripts/CameraTrackController.cs
@@ -7,6 +7,7 @@ public class CameraTrackController : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private CinemachineVirtualCamera _camera;
+    [SerializeField] private CinemachineVirtualCamera _cameraArea1;
     [SerializeField] private CinemachinePathBase _track;
     [SerializeField] private BoxCollider _collider;
     private GameObject _player;
@@ -15,31 +16,40 @@ public class CameraTrackController : MonoBehaviour
     {
         _collider.enabled = false;
         _player = FindObjectOfType<PlayerController>().gameObject;
+        CinemachineTrackedDolly cinemachineTrackedDolly = _camera.GetCinemachineComponent<CinemachineTrackedDolly>();
+        if (cinemachineTrackedDolly != null)
+        {
+            _camera.m_Follow = _player.transform;
+        }
+        if (_cameraArea1 != null)
+        {
+            _cameraArea1.MoveToTopOfPrioritySubqueue();
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         _collider.enabled = true;
-        if (_track != null)
-        {
-            _camera.AddCinemachineComponent<CinemachineTrackedDolly>();
-            CinemachineTrackedDolly cinemachineTrackedDolly = _camera.GetComponent<CinemachineTrackedDolly>();
-            cinemachineTrackedDolly.m_Path = _track;
-            _camera.m_Follow = _player.transform;
-            cinemachineTrackedDolly.m_AutoDolly.m_Enabled = true;
-            cinemachineTrackedDolly.m_XDamping = 1;
-            cinemachineTrackedDolly.m_YDamping = 1;
-            cinemachineTrackedDolly.m_ZDamping = 1;
-            cinemachineTrackedDolly.m_PathOffset = new Vector3(0, 10, 0);
-            cinemachineTrackedDolly.m_AutoDolly.m_SearchRadius = 5;
-        }
-        else
-        {
-            _camera.DestroyCinemachineComponent<CinemachineTrackedDolly>();
-            _camera.m_Follow = transform;
-            CinemachineFramingTransposer cinemachineFramingConposer = _camera.AddCinemachineComponent<CinemachineFramingTransposer>();
-            cinemachineFramingConposer.m_UnlimitedSoftZone = true;
-            cinemachineFramingConposer.m_TargetMovementOnly = false;
-        }
+        _camera.MoveToTopOfPrioritySubqueue();
+        //if (_track != null)
+        //{
+        //    CinemachineTrackedDolly cinemachineTrackedDolly = _camera.AddCinemachineComponent<CinemachineTrackedDolly>();
+        //    cinemachineTrackedDolly.m_Path = _track;
+        //    _camera.m_Follow = _player.transform;
+        //    cinemachineTrackedDolly.m_AutoDolly.m_Enabled = true;
+        //    cinemachineTrackedDolly.m_XDamping = 1;
+        //    cinemachineTrackedDolly.m_YDamping = 1;
+        //    cinemachineTrackedDolly.m_ZDamping = 1;
+        //    cinemachineTrackedDolly.m_PathOffset = new Vector3(0, 10, 0);
+        //    cinemachineTrackedDolly.m_AutoDolly.m_SearchRadius = 5;
+        //}
+        //else
+        //{
+        //    _camera.DestroyCinemachineComponent<CinemachineTrackedDolly>();
+        //    _camera.m_Follow = transform;
+        //    CinemachineFramingTransposer cinemachineFramingConposer = _camera.AddCinemachineComponent<CinemachineFramingTransposer>();
+        //    cinemachineFramingConposer.m_UnlimitedSoftZone = true;
+        //    cinemachineFramingConposer.m_TargetMovementOnly = false;
+        //}
     }
 }
